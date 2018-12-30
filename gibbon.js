7d64142d552cc4555cf360d4a58415bc24d04ed1
Zakery Clarke <zakerytclarke@gmail.com>
2018-12-29 20:57:52 -0700

    Fixed page id mistakes, fixed success page

--- gibbon.js
+++ gibbon.js
@@ -12,7 +12,7 @@ var getdb=new XMLHttpRequest();
 getdb.onreadystatechange=function(){
   if(this.readyState==4&&this.status==200){
     db=JSON.parse(this.responseText);
-    //For Debugging DB
+    //For Debugging
     //console.log(db);
     loadpage();
   }
@@ -71,67 +71,15 @@ function loadpage(){
             c[i].innerHTML="<div id='gibboneditor' style='width:80%;margin-left:10%;text-align:center;background-color:#3e3f42;padding:20px;box-sizing:border-box;border-radius:15px;'><input id='gibbontitle' type='text' placeholder='Title' style='width:35%;border-radius:15px;padding:1vmin;'><input id='gibbonpat' type='text'placeholder='Personal Access Token' style='width:35%;border-radius:15px;padding:1vmin;'><select id='gibbontype'><option value='page'>Page</option><option value='blog'>Blog</option><option value='hidden'>Hidden</option></select><div style='padding-left:10px;padding-right:10px;margin-top:10px;margin-bottom:10px;text-align:left;display:inline-block;vertical-align:middle;background-color:#acadaf;border-radius:10vmin;'><img src='icons/bold.png' onclick='insert(&quot;<b></b>&quot;);' title='Bold'><img src='icons/italic.png' onclick='insert(&quot;<i></i>&quot;);' title='Italic'><img src='icons/underline.png' onclick='insert(&quot;<u></u>&quot;);' title='Underline'><img src='icons/title.png' onclick='insert(&quot;\n<h1></h1>&quot;);' title='Insert Header'><img src='icons/ul.png' onclick='insert(&quot;\n<ul>\n<li></li>\n</ul>&quot;);' title='Bulleted List'><img src='icons/ol.png' onclick='insert(&quot;\n<ol>\n<li></li>\n</ol>&quot;);' title='Numbered List'><img src='icons/addphoto.png' onclick='insert(&quot;\n<img>&quot;);' title='Insert Image'><img src='icons/attatchment.png' onclick='insert(&quot;\n<a></a>&quot;);' class='menu' title='Insert Link'></div><div style='padding-left:10px;padding-right:10px;margin-top:10px;margin-bottom:10px;text-align:left;display:inline-block;vertical-align:middle;background-color:#acadaf;border-radius:10vmin;'><img src='icons/uploadmedia.png' onclick='uploadmedia();' class='menu' title='Upload Media'><img src='icons/save.png' onclick='savepage();' class='menu' title='Save Changes'></div><textarea id='gibboncontent' rows='15' style='width:100%;padding:1vmin;border-radius:15px;'></textarea><br></div>";
     }
   }else{
-    if(page in db.pages){
-      if(page=="success"){
-        var c=document.getElementsByClassName("site-title");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=db.title;
-        }
-        c=document.getElementsByClassName("title");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML="Successful Update";
-        }
-        var menu="";
-        for(var key in db.pages){
-          menu+="<a href='https://"+db.user+".github.io/"+db.repo+"/?"+key+"'>"+db.pages[key].title+"</a>";
-        }
-        c=document.getElementsByClassName("menu");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=menu;
-        }
-        c=document.getElementsByClassName("date");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML="";
-        }
-        c=document.getElementsByClassName("content");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML="You have successfully updated your content";
-        }
-      }else{
-        var c=document.getElementsByClassName("site-title");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=db.title;
-        }
-        c=document.getElementsByClassName("title");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=db.pages[page].title;
-        }
-        var menu="";
-        for(var key in db.pages){
-          menu+="<a href='https://"+db.user+".github.io/"+db.repo+"/?"+key+"'>"+db.pages[key].title+"</a>";
-        }
-        c=document.getElementsByClassName("menu");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=menu;
-        }
-        c=document.getElementsByClassName("date");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=db.pages[page].date;
-        }
-        c=document.getElementsByClassName("content");
-        for(var i=0;i<c.length;i++){
-          c[i].innerHTML=db.pages[page].content;
-        }
-      }
 
-    }else{
+    if(page=="success"){
       var c=document.getElementsByClassName("site-title");
       for(var i=0;i<c.length;i++){
         c[i].innerHTML=db.title;
       }
       c=document.getElementsByClassName("title");
       for(var i=0;i<c.length;i++){
-        c[i].innerHTML="404 Error";
+        c[i].innerHTML="Successful Update";
       }
       var menu="";
       for(var key in db.pages){
@@ -147,9 +95,62 @@ function loadpage(){
       }
       c=document.getElementsByClassName("content");
       for(var i=0;i<c.length;i++){
-        c[i].innerHTML="You have reached this page in error.";
+        c[i].innerHTML="You have successfully updated your content";
+      }
+    }else{
+      if(page in db.pages){
+          var c=document.getElementsByClassName("site-title");
+          for(var i=0;i<c.length;i++){
+            c[i].innerHTML=db.title;
+          }
+          c=document.getElementsByClassName("title");
+          for(var i=0;i<c.length;i++){
+            c[i].innerHTML=db.pages[page].title;
+          }
+          var menu="";
+          for(var key in db.pages){
+            menu+="<a href='https://"+db.user+".github.io/"+db.repo+"/?"+key+"'>"+db.pages[key].title+"</a>";
+          }
+          c=document.getElementsByClassName("menu");
+          for(var i=0;i<c.length;i++){
+            c[i].innerHTML=menu;
+          }
+          c=document.getElementsByClassName("date");
+          for(var i=0;i<c.length;i++){
+            c[i].innerHTML=db.pages[page].date;
+          }
+          c=document.getElementsByClassName("content");
+          for(var i=0;i<c.length;i++){
+            c[i].innerHTML=db.pages[page].content;
+          }
+        }else{
+        var c=document.getElementsByClassName("site-title");
+        for(var i=0;i<c.length;i++){
+          c[i].innerHTML=db.title;
+        }
+        c=document.getElementsByClassName("title");
+        for(var i=0;i<c.length;i++){
+          c[i].innerHTML="404 Error";
+        }
+        var menu="";
+        for(var key in db.pages){
+          menu+="<a href='https://"+db.user+".github.io/"+db.repo+"/?"+key+"'>"+db.pages[key].title+"</a>";
+        }
+        c=document.getElementsByClassName("menu");
+        for(var i=0;i<c.length;i++){
+          c[i].innerHTML=menu;
+        }
+        c=document.getElementsByClassName("date");
+        for(var i=0;i<c.length;i++){
+          c[i].innerHTML="";
+        }
+        c=document.getElementsByClassName("content");
+        for(var i=0;i<c.length;i++){
+          c[i].innerHTML="You have reached this page in error.";
+        }
       }
     }
+
   }
 }
 
@@ -174,7 +175,7 @@ function savepage(){
   var pat=document.getElementById("gibbonpat").value;
 
   db.pages[page]={};
-  db.pages[page].title=title;
+  db.pages[page].title=id;
   db.pages[page].content=content;
   db.pages[page].date=today;
   db.pages[page].type=type;
