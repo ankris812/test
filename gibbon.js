c5bb8e199e8a77edbfc993e2cb7670ce5566d5fb
Zakery Clarke <zakerytclarke@gmail.com>
2019-04-28 22:27:53 -0600

    Bug Fixes

--- gibbon.js
+++ gibbon.js
@@ -10,21 +10,24 @@ console.log("Gibbon CMS V2.0.2");
 var getpage=new XMLHttpRequest();
   getpage.onreadystatechange=function() {
   var md;
-  if(this.readyState==4&&this.status==200){
-			md=this.responseText;
+  if(this.readyState == 4 && this.status == 200){
+			md = this.responseText;
    }else{
    	md="# 404 Error";
    }
    var converter = new showdown.Converter();
 		var html = converter.makeHtml(md);
-		document.getElementById("main").innerHTML+=html;
- 
+    var locs = document.getElementsByClassName("content");
+		for(var i=0;i<locs.length;i++){
+      locs[i].innerHTML=md;
+    }
 };
 
 var page=window.location.search.slice(1);
 if(page==""){
   page="index";
 }
-var db={};
+
+
 getpage.open("GET",page+".md",true);
 getpage.send();
